25713378928a7bde1df03d32c24ab45a2b075487
tzxiaomo <1431128779@qq.com>
2018-07-13 13:19:57 +0800

    Canvas实现小球球效果，可以用作网页背景

---
+++ js/index.js
@@ -0,0 +1,63 @@
+  var w, h, $babbleBall = document.querySelector(".babbleBall"),
+      _babbleBallCon, _colors = [],
+      _babbleBallRunTime = 1000 / 60,
+      _babbleBallArr = [];
+  ~~ function setSize() {
+      window.onresize = arguments.callee;
+      w = window.innerWidth;
+      h = window.innerHeight;
+      $babbleBall.width = w;
+      $babbleBall.height = h;
+  }();
+
+  _colors = ["#81C2B6", "#8192D6", "#D9B3E6", "#DEF7A1", "#83FCD8"];
+
+  function random(min, max) {
+      return Math.random() * (max - min) + min;
+  }
+  _babbleBallCon = $babbleBall.getContext("2d");
+
+  function Babble() {};
+  Babble.prototype = {
+      init: function() {
+          this.x = random(0, w);
+          this.y = random(0, h);
+          this.r = random(1, 3);
+          this.color = _colors[Math.floor(random(0, 5))];
+          this.vx = random(-1, 1);
+          this.vy = random(-1, 1);
+      },
+      draw: function() {
+          _babbleBallCon.beginPath();
+          _babbleBallCon.fillStyle = this.color;
+          _babbleBallCon.arc(this.x, this.y, this.r, 0, Math.PI * 2);
+          _babbleBallCon.fill();
+      },
+      move: function() {
+          this.x += this.vx;
+          this.y += this.vy;
+          if ((this.x - this.r) < 0 || (this.x + this.r) > w) {
+              this.vx = -this.vx;
+          }
+          if ((this.y - this.r) < 0 || (this.y + this.r) > h) {
+              this.vy = -this.vy;
+          }
+          this.draw();
+      }
+  }
+
+  function create(num) {
+      for (var i = 0; i < num; i++) {
+          var _newBabbleBall = new Babble();
+          _newBabbleBall.init();
+          _newBabbleBall.draw();
+          _babbleBallArr.push(_newBabbleBall);
+      }
+  }
+  create(1314);
+  setInterval(function() {
+      _babbleBallCon.clearRect(0, 0, w, h);
+      for (var item of _babbleBallArr) {
+          item.move();
+      }
+  }, _babbleBallRunTime)
